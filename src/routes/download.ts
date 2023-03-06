03ea040597e4549f730e87d8949a66311ffebced
ravish <ravish1729@gmail.com>
2023-03-07 00:35:13 +0530

    import updated

--- src/routes/download.ts
+++ src/routes/download.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import validate from '../middleware/validate'
 import validator from '../middleware/validators'
-import DownloadController from '../controller/DownloadController'
+import { download_car } from '../controller/DownloadController'
 
 const router = express.Router()
 
 router.get(
   '/download_car',
   validate(validator.pieceCIDSchema, { query: true }),
-  DownloadController.download_car
+  download_car
 )
 
 export default router
