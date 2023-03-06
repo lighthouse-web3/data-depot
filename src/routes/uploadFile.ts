d613941d07eeb873dac8e30d21fced72b936a253
ravish <ravish1729@gmail.com>
2023-03-07 00:35:25 +0530

    import fixed

--- src/routes/uploadFile.ts
+++ src/routes/uploadFile.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import { uploadMultipleFile } from '../middleware/upload'
-import FileUploadController from '../controller/FileUploadController'
+import { upload_files } from '../controller/FileUploadController'
 const router = express.Router()
 
 router.post(
   '/upload_files',
   uploadMultipleFile,
-  FileUploadController.upload_multiple_files
+  upload_files
 )
 
 export default router
