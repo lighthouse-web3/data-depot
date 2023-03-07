e940a20b9bd85a167168660bec746a3e66ffc1d3
xlassix <awhy14539@gmail.com>
2023-03-07 15:29:36 +0100

    auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,23 @@
+import { verifyToken } from '../controller/authController/service'
+import type { Request, Response, NextFunction } from 'express'
+
+export const protect = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith(`Bearer `)
+    ) {
+      return res.status(401).end()
+    }
+    const data = await verifyToken(
+      req.headers.authorization.split(`Bearer `)[1]
+    )
+    if (!data) {
+      req.user = data
+      next()
+    }
+  } catch (err: any) {
+    console.log(err.message)
+    return res.status(401).end()
+  }
+}
