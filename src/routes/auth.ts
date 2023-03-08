8495e323825707f074ccaefcac913c8e87e4888c
ravish <ravish1729@gmail.com>
2023-03-09 02:08:47 +0530

    rename authGoogle->auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { githubOauthHandler } from '../controller/authController'
+import { githubOauthHandler, logoutHandler } from '../controller/authController'
 
 const router = express.Router()
 
 router.get('/oauth/github', githubOauthHandler)
+router.get('/logout', logoutHandler)
 
 export default router
