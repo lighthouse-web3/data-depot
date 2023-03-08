1b5c4d2dff41d038e56a3d3ae5d84167e938a315
ravish <ravish1729@gmail.com>
2023-03-09 03:57:00 +0530

    renamed gerFileList->getUserUploads

--- src/databaseOperations/getUserUploads.ts
+++ src/databaseOperations/getUserUploads.ts
@@ -1,10 +1,10 @@
 import dbbClient from './ddbClient'
 import { unmarshall } from '@aws-sdk/util-dynamodb'
-import { ScanCommand } from '@aws-sdk/client-dynamodb'
+import { QueryCommand } from '@aws-sdk/client-dynamodb'
 import { carRecordTable } from '../utils/constants'
 import { DatabaseError } from '../errors'
 
-export default async (pageNo: number) => {
+export default async (userName: String, pageNo: number) => {
   try {
     let records = null
     let count = 0
@@ -15,11 +15,17 @@ export default async (pageNo: number) => {
     do {
       const params: any = {
         TableName: carRecordTable,
-        Limit: 2000,
+        IndexName: 'userName-index',
+        ScanIndexForward: false,
+        KeyConditionExpression: 'userName = :u',
+        ExpressionAttributeValues: {
+            ':u': {S: userName},
+        },
+        Limit: 20000,
         ExclusiveStartKey: exclusiveStartKey,
       }
 
-      records = await dbbClient.send(new ScanCommand(params))
+      records = await dbbClient.send(new QueryCommand(params))
       count += 1
       exclusiveStartKey = records.LastEvaluatedKey
       if (!exclusiveStartKey && pageNo > count) {
