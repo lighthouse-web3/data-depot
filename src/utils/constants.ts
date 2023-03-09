88610f1a89eac40405696d6614234c2076ccb1af
ravish <ravish1729@gmail.com>
2023-03-09 22:00:55 +0530

    user record table added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 const totalNumberOfFiles = 20
 const maxFileSize = 5368709120
 const carRecordTable = 'car-file-records'
+const userRecordTable = 'data-depot-user-records'
 
-export { carRecordTable, maxFileSize, totalNumberOfFiles }
+export { carRecordTable, maxFileSize, totalNumberOfFiles, userRecordTable }
