92cab93af1ce86d41e73d144a74fbc7ec615b18d
ravish <ravish1729@gmail.com>
2023-03-10 01:00:38 +0530

    user info

---
+++ src/databaseOperations/getUserDetails.ts
@@ -0,0 +1,22 @@
+import chalk from 'chalk'
+import dbbClient from './ddbClient'
+import { userRecordTable } from '../utils/constants'
+
+export default async (userName: string) => {
+  try {
+    const params = {
+      TableName: userRecordTable,
+      Key: {
+        userName: userName
+      },
+    }
+
+    const record = await dbbClient.get(params)
+    return record.Item
+  } catch (error: any) {
+    console.log(
+      chalk.yellow('User Detail Fetch Error: ') + chalk.red(error.message)
+    )
+    return {}
+  }
+}
