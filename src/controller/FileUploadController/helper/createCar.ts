1178a552a794ce2aef047e0259ba9ec32ea3404e
ravish <ravish1729@gmail.com>
2023-03-10 15:01:05 +0530

    updated

--- src/controller/FileUploadController/helper/createCar.ts
+++ src/controller/FileUploadController/helper/createCar.ts
@@ -1,15 +1,25 @@
 import fs from 'fs'
 import chalk from 'chalk'
+import { uploadS3 } from './uploadS3'
 import { execute } from '../../../utils/execute'
 import updateFileRecord from '../../../databaseOperations/updateFileRecord'
 
-export const createCar = async (fileId: any) => {
+export const createCar = async (fileId: string, fileName: string) => {
   const log = console.log
   try {
+    // Create CAR
     const car: any = await execute(
-      `generate-car --single -i ${process.cwd()}/uploads/${fileId} -o ${process.cwd()}/carGenerated -p ${process.cwd()}`
+      `generate-car --single -i ${process.cwd()}/uploads/${fileId}/${fileName} -o ${process.cwd()}/carGenerated -p ${process.cwd()}`
     )
     const jsonResponse = JSON.parse(car)
+
+    // Push CAR to S3
+    const pushToS3 = await uploadS3(jsonResponse['PieceCid'])
+    if(!pushToS3){
+      throw new Error("Failed to save file to s3")
+    }
+
+    // Create DB record
     const _ = await updateFileRecord({
       id: fileId,
       payloadCid: jsonResponse['DataCid'],
@@ -20,6 +30,8 @@ export const createCar = async (fileId: any) => {
 
     // Remove Uploaded file
     fs.rmSync(`${process.cwd()}/uploads/${fileId}`, { recursive: true })
+    // Remove car file from disk
+    fs.rmSync(`${process.cwd()}/carGenerated/${jsonResponse['PieceCid']}`, { recursive: true })
     return
   } catch (error) {
     log(chalk.red('Error creating car: ') + error)
