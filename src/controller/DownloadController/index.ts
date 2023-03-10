0b5fc9c06a7d910c1b40ef739638aee11c13a411
ravish <ravish1729@gmail.com>
2023-03-10 15:00:22 +0530

    updated

--- src/controller/DownloadController/index.ts
+++ src/controller/DownloadController/index.ts
@@ -1,10 +1,22 @@
 import chalk from 'chalk'
-import type { NextFunction, Request, Response } from 'express'
+import { carFileBucket } from '../../utils/constants'
+import type { NextFunction, Response } from 'express'
+import { s3Connect } from '../FileUploadController/helper/s3Connect'
 
 export const download_car = async (req: any, res: Response, next: NextFunction) => {
   try {
-    const file = `./carGenerated/${req.query.piece_cid}`
-    res.download(file)
+    const s3 = await s3Connect()
+    if(!s3){
+      throw new Error()
+    }
+
+    res.attachment(req.query.piece_cid)
+    const options = {
+      Bucket: carFileBucket,
+      Key: req.query.piece_cid,
+    }
+    const fileStream = s3.getObject(options).createReadStream()
+    fileStream.pipe(res)
   } catch (error: any) {
     console.log(chalk.red(`Error: Failed to download- ${req.query.piece_cid}`))
     next(error)
