6668f8e69c7ae43083644949d2cca8e59059b790
ravish <ravish1729@gmail.com>
2023-03-12 23:06:21 +0530

    upload path update

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,14 +1,15 @@
 import fs from 'fs'
 import multer from 'multer'
 import { v4 } from 'uuid'
+import config from '../config'
 import { totalNumberOfFiles, maxFileSize } from '../utils/constants'
 import { Request, Response } from 'express'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const fileId = v4()
-    fs.mkdirSync('uploads/' + fileId)
-    cb(null, 'uploads/' + fileId)
+    fs.mkdirSync(`${config.uploadPath}/${fileId}`)
+    cb(null, `${config.uploadPath}/${fileId}`)
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
