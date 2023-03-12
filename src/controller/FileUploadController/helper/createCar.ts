48eb42a276ff348a2946845de41fd95723ebff03
ravish <ravish1729@gmail.com>
2023-03-12 22:17:23 +0530

    car size added

--- src/controller/FileUploadController/helper/createCar.ts
+++ src/controller/FileUploadController/helper/createCar.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import chalk from 'chalk'
+import config from '../../../config'
 import { uploadS3 } from './uploadS3'
 import { execute } from '../../../utils/execute'
 import updateFileRecord from '../../../databaseOperations/updateFileRecord'
@@ -8,12 +9,15 @@ export const createCar = async (fileId: string, fileName: string) => {
   const log = console.log
   try {
     // Create CAR
+    const nameFormat = fileName.replace(/ /g, '\\ ')
     const car: any = await execute(
-      `generate-car --single -i ${process.cwd()}/uploads/${fileId}/${fileName} -o ${process.cwd()}/carGenerated -p ${process.cwd()}`
+      `generate-car --single -i ${config.uploadPath}/${fileId}/${nameFormat} -o ${config.carPath} -p ${config.uploadPath}/${fileId}`
     )
     const jsonResponse = JSON.parse(car)
-
-    // Push CAR to S3
+    const carSize: any = await execute(
+      `stat --format="%s" ${config.carPath}/${jsonResponse['PieceCid']}.car`
+    )
+    // // Push CAR to S3
     const pushToS3 = await uploadS3(jsonResponse['PieceCid'])
     if(!pushToS3){
       throw new Error("Failed to save file to s3")
@@ -24,6 +28,7 @@ export const createCar = async (fileId: string, fileName: string) => {
       id: fileId,
       payloadCid: jsonResponse['DataCid'],
       pieceCid: jsonResponse['PieceCid'],
+      carSize: parseInt(carSize.trim()),
       pieceSize: jsonResponse['PieceSize'],
       fileStatus: 'CAR Created',
     })
@@ -31,7 +36,7 @@ export const createCar = async (fileId: string, fileName: string) => {
     // Remove Uploaded file
     fs.rmSync(`${process.cwd()}/uploads/${fileId}`, { recursive: true })
     // Remove car file from disk
-    fs.rmSync(`${process.cwd()}/carGenerated/${jsonResponse['PieceCid']}`, { recursive: true })
+    fs.rmSync(`${process.cwd()}/carGenerated/${jsonResponse['PieceCid']}.car`, { recursive: true })
     return
   } catch (error) {
     log(chalk.red('Error creating car: ') + error)
