9daaec7e333979e1a7d8ef088f1fe33c41e49e96
ravish1729 <ravish1729@gmail.com>
2024-01-27 07:41:12 +0530

    head request support

--- src/controller/DownloadController/index.ts
+++ src/controller/DownloadController/index.ts
@@ -3,20 +3,69 @@ import { carFileBucket } from '../../utils/constants'
 import type { NextFunction, Response } from 'express'
 import { s3Connect } from '../FileUploadController/helper/s3Connect'
 
-export const download_car = async (req: any, res: Response, next: NextFunction) => {
+export const download_car_head = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
   try {
+    console.log(chalk.yellow(`Miner getting file metadata: ${req.query.fileId}`))
     const s3 = await s3Connect()
-    if(!s3){
+    if (!s3) {
       throw new Error()
     }
 
-    res.attachment(req.query.fileId)
+    const options = {
+      Bucket: carFileBucket,
+      Key: req.query.fileId,
+    }
+
+    s3.headObject(options, (err, data) => {
+      if (err) {
+        console.log(chalk.red(`Error: Failed to get metadata- ${req.query.fileId}`))
+        next(err)
+      } else {
+	res.writeHead(200, {
+          'Content-Length': data.ContentLength,
+          'Last-Modified': data.LastModified?.toUTCString(),
+          'Content-Type': data.ContentType,
+        })
+        res.end()
+      }
+    })
+  } catch (error: any) {
+    console.log(chalk.red(`Error: Failed to get metadata- ${req.query.fileId}`))
+    next(error)
+  }
+}
+
+export const download_car = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log(chalk.yellow(`Miner downloading file: ${req.query.fileId}`))
+    const s3 = await s3Connect()
+    if (!s3) {
+      throw new Error()
+    }
+
+//    res.attachment(req.query.fileId)
+    res.setHeader('Content-Disposition', 'attachment; filename=' + req.query.fileId)
+
     const options = {
       Bucket: carFileBucket,
       Key: req.query.fileId,
     }
     const fileStream = s3.getObject(options).createReadStream()
-    fileStream.pipe(res)
+//    fileStream.pipe(res)
+    fileStream.on('data', (chunk) => {
+      res.write(chunk)
+    })
+    fileStream.on('end', () => {
+      res.end()
+    })
   } catch (error: any) {
     console.log(chalk.red(`Error: Failed to download- ${req.query.fileId}`))
     next(error)
--- src/routes/download.ts
+++ src/routes/download.ts
@@ -1,9 +1,15 @@
 import express from 'express'
 import validate from '../middleware/validate'
 import validator from '../middleware/validators'
-import { download_car } from '../controller/DownloadController'
+import { download_car, download_car_head } from '../controller/DownloadController'
 
 const router = express.Router()
+
+router.head(
+  '/download_car',
+  validate(validator.fileIdSchema, { query: true }),
+  download_car_head
+)
 
 router.get(
   '/download_car',
