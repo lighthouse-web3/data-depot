430405f80856b067489de8ad7b0c1184c046065f
lordforever <shashanktrivedi1917@gmail.com>
2024-06-01 19:36:57 +0530

    local serve

--- src/controller/DownloadController/index.ts
+++ src/controller/DownloadController/index.ts
@@ -1,7 +1,8 @@
+import fs from 'fs'
 import chalk from 'chalk'
-import { carFileBucket } from '../../utils/constants'
+import path from 'path'
+import config from '../../config' // Assuming config is exported from this path
 import type { NextFunction, Response } from 'express'
-import { s3Connect } from '../FileUploadController/helper/s3Connect'
 
 export const download_car_head = async (
   req: any,
@@ -9,29 +10,28 @@ export const download_car_head = async (
   next: NextFunction
 ) => {
   try {
-    console.log(chalk.yellow(`Miner getting file metadata: ${req.query.fileId}`))
-    const s3 = await s3Connect()
-    if (!s3) {
-      throw new Error()
-    }
+    const fileName = req.query.fileId
+    const filePath = path.join(config.carPath, fileName)
+    chalk.yellow(`Miner getting file metadata: ${req.query.fileId}`)
 
-    const options = {
-      Bucket: carFileBucket,
-      Key: req.query.fileId,
-    }
-
-    s3.headObject(options, (err, data) => {
+    fs.stat(filePath, (err, stats) => {
       if (err) {
-        console.log(chalk.red(`Error: Failed to get metadata- ${req.query.fileId}`))
-        next(err)
-      } else {
-	res.writeHead(200, {
-          'Content-Length': data.ContentLength,
-          'Last-Modified': data.LastModified?.toUTCString(),
-          'Content-Type': data.ContentType,
-        })
-        res.end()
+        if (err.code === 'ENOENT') {
+          res.status(404).send('File not found')
+        } else {
+          res.status(500).send('Internal server error')
+        }
+        console.log(
+          chalk.red(`Error: Failed to get metadata- ${req.query.fileId}`)
+        )
+        return
       }
+
+      res.setHeader('Content-Disposition', 'attachment; filename=' + fileName)
+      res.setHeader('Content-Length', stats.size.toString())
+      res.setHeader('Last-Modified', stats.mtime.toUTCString())
+      res.setHeader('Content-Type', 'application/octet-stream')
+      res.status(200).end()
     })
   } catch (error: any) {
     console.log(chalk.red(`Error: Failed to get metadata- ${req.query.fileId}`))
@@ -45,26 +45,27 @@ export const download_car = async (
   next: NextFunction
 ) => {
   try {
+    const fileName = `${req.query.fileId}`
+    const filePath = path.join(config.carPath, fileName)
     console.log(chalk.yellow(`Miner downloading file: ${req.query.fileId}`))
-    const s3 = await s3Connect()
-    if (!s3) {
-      throw new Error()
-    }
 
-//    res.attachment(req.query.fileId)
-    res.setHeader('Content-Disposition', 'attachment; filename=' + req.query.fileId)
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          res.status(404).send('File not found')
+        } else {
+          res.status(500).send('Internal server error')
+        }
+        console.log(chalk.red(`Error: Failed to download- ${req.query.fileId}`))
+        return
+      }
 
-    const options = {
-      Bucket: carFileBucket,
-      Key: req.query.fileId,
-    }
-    const fileStream = s3.getObject(options).createReadStream()
-//    fileStream.pipe(res)
-    fileStream.on('data', (chunk) => {
-      res.write(chunk)
-    })
-    fileStream.on('end', () => {
-      res.end()
+      const fileStream = fs.createReadStream(filePath)
+      res.setHeader('Content-Disposition', 'attachment; filename=' + fileName)
+      res.setHeader('Content-Length', stats.size.toString())
+      res.setHeader('Content-Type', 'application/octet-stream')
+      res.status(200)
+      fileStream.pipe(res)
     })
   } catch (error: any) {
     console.log(chalk.red(`Error: Failed to download- ${req.query.fileId}`))
